09c0a0d4e070870c494d613126849ea77740da70
Planeurzik <38111551+Planeurzik@users.noreply.github.com>
2025-07-15 02:07:18 +0000

    Deploying to gh-pages from @ Planeurzik/planeurzik.github.io@c0db1f880429431aa16def1f82fc813f93411564 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%70%68%69%6C%69%70%70%65%67%72%61%74%69%61%73@%6F%72%61%6E%67%65.%66%72", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Planeurzik", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -509,6 +516,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/philippe-gratias-quiquandon-570a98253", "_blank");
         },
       },{
         id: 'social-rss',
@@ -523,13 +537,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
